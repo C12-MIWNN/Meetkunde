9f5d73fb27b33020af661cec79c97b644ad367a0
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-09-29 11:02:47 +0200

    4.3 Rechthoek erft nu ook van Figuur

--- src/model/Rechthoek.java
+++ src/model/Rechthoek.java
@@ -4,23 +4,20 @@ package model;
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
  * Beschrijft een rechthoek en diens eigenschappen
  */
-public class Rechthoek {
+public class Rechthoek extends Figuur {
     private static final int DEFAULT_LENGTE = 2;
     private static final int DEFAULT_BREEDTE = 1;
     private static final String DEFAULT_KLEUR = "Pegasus Chill";
 
-    private static final double GRENSWAARDE_GROOT_FIGUUR = 100.0;
-
     private double lengte;
     private double breedte;
     private Punt hoekpuntLinksBoven;
-    private String kleur;
 
     public Rechthoek(double lengte, double breedte, Punt hoekpuntLinksBoven, String kleur) {
+        super(kleur);
         this.lengte = lengte;
         this.breedte = breedte;
         this.hoekpuntLinksBoven = hoekpuntLinksBoven;
-        this.kleur = kleur;
     }
 
     public Rechthoek(double lengte, double breedte) {
@@ -35,20 +32,14 @@ public class Rechthoek {
         return "Een rechthoek is een vierhoek met vier rechte hoeken.";
     }
 
+    @Override
     public double geefOmtrek() {
         return 2 * (lengte + breedte);
     }
 
+    @Override
     public double geefOppervlakte() {
         return lengte * breedte;
-    }
-
-    public String vertelOverDeGrootte() {
-        if (geefOppervlakte() < GRENSWAARDE_GROOT_FIGUUR) {
-            return "Zij zijn groot en ik ben klein en dat is niet eerlijk!";
-        } else {
-            return "Deal with it!";
-        }
     }
 
     public Punt getHoekpuntLinksBoven() {
