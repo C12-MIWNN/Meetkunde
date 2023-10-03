13ad807398ea88909dd1f12e3ddf2d9aa9effa93
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2023-10-03 13:52:10 +0200

    9.1 effecienter opgeschreven

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -5,8 +5,6 @@ import model.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -16,32 +14,22 @@ import java.util.Scanner;
 public class MeetkundeLauncher {
 
     public static void main(String[] args) {
-        ArrayList<String> regelsUitHetBestand = new ArrayList<>();
+        ArrayList<Rechthoek> rechthoeken = new ArrayList<>();
 
-        File rechthoekenBestand = new File("resources/Rechthoek.csv");
-
-        try {
-            Scanner invoerBestand = new Scanner(rechthoekenBestand);
-
+        try (Scanner invoerBestand = new Scanner(new File("resources/Rechthoek.csv"))) {
             while (invoerBestand.hasNextLine()) {
-                regelsUitHetBestand.add(invoerBestand.nextLine());
+                String[] waardenVanRechthoek = invoerBestand.nextLine().split(",");
+
+                double lengte = Double.parseDouble(waardenVanRechthoek[0]);
+                double breedte = Double.parseDouble(waardenVanRechthoek[1]);
+                double xCoordinaat = Double.parseDouble(waardenVanRechthoek[2]);
+                double yCoordinaat = Double.parseDouble(waardenVanRechthoek[3]);
+                String kleur = waardenVanRechthoek[4];
+
+                rechthoeken.add(new Rechthoek(lengte, breedte, new Punt(xCoordinaat, yCoordinaat), kleur));
             }
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.println("Het bestand is niet gevonden");
-        }
-
-        ArrayList<Rechthoek> rechthoeken = new ArrayList<>();
-
-        for (String regelMetRechthoek : regelsUitHetBestand) {
-            String[] waardenVanRechthoek = regelMetRechthoek.split(",");
-
-            double lengte = Double.parseDouble(waardenVanRechthoek[0]);
-            double breedte = Double.parseDouble(waardenVanRechthoek[1]);
-            double xCoordinaat = Double.parseDouble(waardenVanRechthoek[2]);
-            double yCoordinaat = Double.parseDouble(waardenVanRechthoek[3]);
-            String kleur = waardenVanRechthoek[4];
-
-            rechthoeken.add(new Rechthoek(lengte, breedte, new Punt(xCoordinaat, yCoordinaat), kleur));
         }
 
         System.out.println(rechthoeken);
